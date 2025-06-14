110093bcc9eb8802cdb85931c111376de6ab00b6
Akanksha Rane <142483716+Akanksha4060@users.noreply.github.com>
2025-06-14 10:00:57 +0530

    Update QuestionService.java

--- QuestionService.java
+++ QuestionService.java
@@ -1,132 +1,60 @@
-package com.example.demo.service;
+package com.quiz.Question.Quiz.Service;
 
-import java.util.List;
-
+import com.quiz.Question.Quiz.Dao.QuestionDao;
+import com.quiz.Question.Quiz.Model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.function.EntityResponse;
-
-import com.example.demo.model.Question;
-import com.example.demo.repository.QuestionRepository;
-
-@Service
-public class QuestionService {
-
-	@Autowired
-	QuestionRepository questionRepository;
-
-	// to get all records  
-	/* --NORMALLY RECORDS WILL DISPLAY
-	 * public List<Question> getAllQuestions() {
-	 * 
-	 * return questionRepository.findAll(); }
-	 */
-	
-	//IF WE PROVIDE WRONG INPUT THEN IT WILL GIVE US EXCEPTION
-	public ResponseEntity<List<Question>> getAllQuestions() {
-		try{
-			return new ResponseEntity<>(questionRepository.findAll(),HttpStatus.OK);
-		}catch(Exception e){
-			e.printStackTrace();
-			}
-		return new ResponseEntity<>(questionRepository.findAll(),HttpStatus.BAD_REQUEST);
-
-		}
-
-	public List<Question> getQuestionByCategory(String cat) {
-
-		return questionRepository.findByCategory(cat);
-	}
-
-	public String addQuestion(Question question) {
-		questionRepository.save(question);
-		return "Success";
-	}
-
-	public void deleteQuestion(int id) {
-		questionRepository.deleteById(id);
-		
-	}
-
-	
-	
-
-}
-package com.example.demo.service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.function.EntityResponse;
-
-import com.example.demo.model.Question;
-import com.example.demo.repository.QuestionRepository;
-
 @Service
 public class QuestionService {
 
-	@Autowired
-	QuestionRepository questionRepository;
+    @Autowired
+    QuestionDao questionDao;
 
-	// to get all records  
-	/* --NORMALLY RECORDS WILL DISPLAY
-	 * public List<Question> getAllQuestions() {
-	 * 
-	 * return questionRepository.findAll(); }
-	 */
-	
-	//IF WE PROVIDE WRONG INPUT THEN IT WILL GIVE US EXCEPTION
-	public ResponseEntity<List<Question>> getAllQuestions() {
-		try{
-			return new ResponseEntity<>(questionRepository.findAll(),HttpStatus.OK);
-		}catch(Exception e){
-			e.printStackTrace();
-			}
-		return new ResponseEntity<>(questionRepository.findAll(),HttpStatus.BAD_REQUEST);
+    public List<Question> getAllQuestions(){
+        return questionDao.findAll();
+    }
+    public List<Question> getQuestionByCategory(String category){
+        return questionDao.findByCategory(category);
+    }
+    public Question getQuestionById(int id){
+        return questionDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Question not found with id: " + id));
+    }
+    public String addQuestion(Question que){
+        questionDao.save(que);
+        return "success";
+    }
+    public void deleteById(int id){
+        questionDao.deleteById(id);
 
-		}
+    }
+    public ResponseEntity<String> updateQuestion(int id, Question updatedQuestion) {
+        if (!questionDao.existsById(id)) {
+            return new ResponseEntity<>("Question not found", HttpStatus.NOT_FOUND);
+        }
 
-	public List<Question> getQuestionByCategory(String cat) {
+        Question existingQuestion = questionDao.findById(id).get();
 
-		return questionRepository.findByCategory(cat);
-	}
+        existingQuestion.setQuestionTitle(updatedQuestion.getQuestionTitle());
+        existingQuestion.setOption1(updatedQuestion.getOption1());
+        existingQuestion.setOption2(updatedQuestion.getOption2());
+        existingQuestion.setOption3(updatedQuestion.getOption3());
+        existingQuestion.setOption4(updatedQuestion.getOption4());
+        existingQuestion.setRightAnswer(updatedQuestion.getRightAnswer());
+        existingQuestion.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
+        existingQuestion.setCategory(updatedQuestion.getCategory());
 
-	public String addQuestion(Question question) {
-		questionRepository.save(question);
-		return "Success";
-	}
+        questionDao.save(existingQuestion);
+        return new ResponseEntity<>("Question updated successfully", HttpStatus.OK);
+    }
 
-	public void deleteQuestion(int id) {
-		questionRepository.deleteById(id);
-		
-	}
 
-	public ResponseEntity<String> updateQuestion(int id, Question updatedQuestion) {
-		if(!questionRepository.existsById(id)) {
-			return new ResponseEntity<>("Question not found",HttpStatus.NOT_FOUND);
-		}
-		
-		Question existingQuestion=questionRepository.findById(id).get();
-		existingQuestion.setQuestionTitle(updatedQuestion.getQuestionTitle());
-		existingQuestion.setOption1(updatedQuestion.getOption1());
-		existingQuestion.setOption2(updatedQuestion.getOption2());
-		existingQuestion.setOption3(updatedQuestion.getOption3());
-		existingQuestion.setOption4(updatedQuestion.getOption4());
-		existingQuestion.setRightAnswer(updatedQuestion.getRightAnswer());
-		existingQuestion.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
-		existingQuestion.setCategory(updatedQuestion.getCategory());
-		
-		questionRepository.save(existingQuestion);
-		return new ResponseEntity<>("Question updated successfully",HttpStatus.OK);
-
-	}
-
-	
-	
 
 }
